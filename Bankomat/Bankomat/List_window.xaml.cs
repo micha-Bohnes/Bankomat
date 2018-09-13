48f79cca7f7c672349c00514de783e857ba7c0a4
Micha Bohnes <micha.bohnes@student.ibz.ch>
2018-09-13 22:40:01 +0200

    Die lezten wpf fenster, (backroundcode (variablen) Fehlen(im gesamten programm))

---
+++ Bankomat/Bankomat/List_window.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Bankomat
+{
+    /// <summary>
+    /// Interaktionslogik für List_window.xaml
+    /// </summary>
+    public partial class List_window : Window
+    {
+        public List_window()
+        {
+            InitializeComponent();
+        }
+
+        private void btnhistory_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Bankomat/Bankomat/Pay_Window.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Bankomat
+{
+    /// <summary>
+    /// Interaktionslogik für Paywindow.xaml
+    /// </summary>
+    public partial class Paywindow : Window
+    {
+        public Paywindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnsaldo_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnpayin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnpayout_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
